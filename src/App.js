0208e329d6f97fcb3e841557387cae8362dcfdd4
myeongakim7 <okmyonga@naver.com>
2022-12-28 17:06:43 +0900

    컴포넌트 안에 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import Box from "./Box";
 
+// APP 컴포넌트 - JSX 문법으로 HTML 생성 / 컴포넌트명은 첫글자 항상 '대문자'
 function App() {
+  const title = "타이틀 입니다";
+  const list = (
+    <ul>
+      <li>루피</li>
+      <li>피카츄</li>
+      <li>강쥐</li>
+    </ul>
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>앱의 이름을 변수로</h1>
+      <h2>{title}</h2>
+      <p>{list}</p>
+      <Box></Box>
+      <Box />
+    </>
   );
 }
 
+//JS 모듈 생성
 export default App;
+// node.js랑 다름
+// export default 함수명
+// 함수를 export(내보내고) -> index.js에서 import(불러온다)한다
---
+++ src/Box.js
@@ -0,0 +1,12 @@
+import React from "react";
+// 위에껀 안적어도 됨
+export default function Box() {
+  return (
+    <div>
+      <h1>Box</h1>
+      <p>box입니다</p>
+    </div>
+  );
+}
+
+// rfc << 입력하면 자동입력
